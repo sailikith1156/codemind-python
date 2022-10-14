a4bcb6bd370fb828d02ace5c56eeed634f77eb66
Sai likith Chirukoti <105732434+sailikith1156@users.noreply.github.com>
2022-10-14 11:00:50 +0530

    for i in range(int(input())):     n=int(input())     m=q=n     while(n):         prime=True         for i in range(2,int(n**(0.5)+1)):             if(n%i==0):                 prime=False         if prime==True:             x=n             break         else:             n+=1     while(m):         prime=True         for i in range(2,int(m**(0.5)+1)):             if(m%i==0):                 prime=False         if prime==True:             y=m             break         else:             m-=1     if(x-q<q-y):         print(x)     else:         print(y)

---
+++ Nearest_Prime.py
@@ -0,0 +1,27 @@
+for i in range(int(input())):
+    n=int(input())
+    m=q=n
+    while(n):
+        prime=True
+        for i in range(2,int(n**(0.5)+1)):
+            if(n%i==0):
+                prime=False
+        if prime==True:
+            x=n
+            break
+        else:
+            n+=1
+    while(m):
+        prime=True
+        for i in range(2,int(m**(0.5)+1)):
+            if(m%i==0):
+                prime=False
+        if prime==True:
+            y=m
+            break
+        else:
+            m-=1
+    if(x-q<q-y):
+        print(x)
+    else:
+        print(y)
